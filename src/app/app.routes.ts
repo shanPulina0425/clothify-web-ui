3e6db62b05e5e569d5ad14018d1809dcca180a00
Shan Pulina <shangtav@gmail.com>
2026-05-07 16:57:05 +0530

    feat: add Home and Checkout components with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'**',redirectTo:''}
+
+];
--- src/app/pages/checkout/checkout.spec.ts
+++ src/app/pages/checkout/checkout.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Checkout } from './checkout';
+import { CheckoutComponent } from './checkout.component';
 
-describe('Checkout', () => {
-  let component: Checkout;
-  let fixture: ComponentFixture<Checkout>;
+describe('CheckoutComponent', () => {
+  let component: CheckoutComponent;
+  let fixture: ComponentFixture<CheckoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Checkout],
+      imports: [CheckoutComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Checkout);
+    fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

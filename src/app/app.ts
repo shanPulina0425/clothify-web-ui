034bdf183ec765d3e921773357de4c486c8067d1
Shan Pulina <shangtav@gmail.com>
2026-05-07 15:21:55 +0530

    feat: create ProductService to manage product data and inject into App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { ProductCard } from './product-card/product-card';
 import { CartSummary } from './cart-summary/cart-summary';
+import { ProductService } from './services/product.service';
 
 export interface Product {
   id: number;
@@ -21,33 +22,9 @@ export interface Product {
 export class App {
   protected readonly title = signal('Clothify Store');
 
-  productList: Product[] = [
-    {
-      id: 1,
-      name: 'Classic White T-Shirt',
-      price: 2500,
-      imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=300&q=80',
-    },
-    {
-      id: 2,
-      name: 'Slim Fit Blue Jeans',
-      price: 5500,
-      imageUrl: 'https://images.unsplash.com/photo-1542272604-787c3835535d?w=300&q=80',
-    },
-    {
-      id: 3,
-      name: 'Oversized Black Hoodie',
-      price: 8500,
-      imageUrl: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=300&q=80',
-    },
-    {
-      id: 4,
-      name: 'Canvas Sneakers',
-      price: 12500,
-      imageUrl: 'https://images.unsplash.com/photo-1549298916-b41d501d3772?w=300&q=80',
-    },
-    
-  ];
+  private productService = inject(ProductService);
+
+  productList: Product[] = this.productService.getProducts();
 
   
 
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../app';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+
+    private products: Product[] = [
+
+      {
+      id: 1,
+      name: 'Classic White T-Shirt',
+      price: 2500,
+      imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=300&q=80',
+    },
+    {
+      id: 2,
+      name: 'Slim Fit Blue Jeans',
+      price: 5500,
+      imageUrl: 'https://images.unsplash.com/photo-1542272604-787c3835535d?w=300&q=80',
+    },
+    {
+      id: 3,
+      name: 'Oversized Black Hoodie',
+      price: 8500,
+      imageUrl: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=300&q=80',
+    },
+    {
+      id: 4,
+      name: 'Canvas Sneakers',
+      price: 12500,
+      imageUrl: 'https://images.unsplash.com/photo-1549298916-b41d501d3772?w=300&q=80',
+    }
+
+    ];
+
+    constructor(){}
+
+    getProducts(): Product[] {
+      return this.products;
+    }
+}
